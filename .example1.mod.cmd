59f4bcdbedec3dac5247b64b901bec95c4a24e97
Jerry <a0979682372@gmail.com>
2023-04-16 21:42:18 +0800

    v1.0.2

--- .example1.mod.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/demo/test10/proc_example/example1.mod := { echo  /home/demo/test10/proc_example/example1.o;  echo; } > /home/demo/test10/proc_example/example1.mod
--- example1.mod
+++
@@ -1,2 +0,0 @@
-/home/demo/test10/proc_example/example1.o
-
